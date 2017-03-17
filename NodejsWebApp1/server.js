e988faed814556a4f8754784109eeff5753f110e
Chris Williams <chris.briana.2016@gmail.com>
2017-03-17 16:19:07 -0500

    add console log

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
+console.log("potato");
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
